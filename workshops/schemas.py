267f41d2a849f2c2cc541daaa861896f416d8c8a
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2025-05-25 19:21:53 -0500

    Added basic HTTP Requests for Workshops, duplication is properly handled

--- workshops/schemas.py
+++ workshops/schemas.py
@@ -1,7 +1,14 @@
 from ninja import Schema
+from django.contrib.auth.models import User
 
-
-class WorkshopSchema(Schema):
-    user: str
+class CreateWorkshopSchema(Schema):
     name: str
     address: str
+    coordinates: str
+
+class WorkshopSchema(Schema):
+    id: int
+    user_id: int
+    name: str
+    address: str
+    coordinates: str
