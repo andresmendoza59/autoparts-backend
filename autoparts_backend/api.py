2d8689617d7beb1906e3c7acb593325314ebdda4
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2025-05-26 17:12:21 -0500

    Added UserSchema

--- autoparts_backend/api.py
+++ autoparts_backend/api.py
@@ -9,19 +9,3 @@ from workshops.api import WorkshopController
 api = NinjaExtraAPI()
 api.register_controllers(NinjaJWTDefaultController)
 api.register_controllers(WorkshopController)
-
-class UserSchema(Schema):
-    username: str
-    is_authenticated: bool
-    email: str = None
-
-
-@api.get("/hello")
-def hello(request) -> str:
-    print(request)
-    return "Hello World!"
-
-
-@api.get("/me", response=UserSchema, auth=JWTAuth())
-def me(request) -> UserSchema:
-    return request.user
--- products/schemas.py
+++ products/schemas.py
@@ -2,4 +2,6 @@ from ninja import Schema
 
 
 class ProductSchema(Schema):
-    ...
+    name: str
+    price: float
+    description: str
